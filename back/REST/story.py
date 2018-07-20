465b7067e20862acaa2a4a5aa2cd52b8c79c3788
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-07-19 22:20:50 -0500

    story creation done well

--- back/REST/story.py
+++ back/REST/story.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, Response
 from aux import *
 from author import getAuthor
+from tag import tagExists
 
 story = Blueprint("story", __name__)
 
@@ -10,15 +11,32 @@ def createStory():
     incoming = request.json
     story = incoming["story"]
     author = incoming["author"]["username"]
+    tags = set(incoming["tags"])
+    # check if every tag exists and if it doesn't, create it
     authorExists = query(
         "match (n:Author) where n.username = $uname return n", {"uname": author})
 
-    if(len(authorExists["results"]) == 0):
+    if(len(authorExists["results"][0]["data"]) == 0):
         return Response("The author does not exist", status=400)
 
-    resp = query("match (a:Author) match (p:Story) where a.username = $uname and id(p) = $pid " +
-                 "create (s :Story $props), (a) -[:Wrote]->(s), (p)<-[:Inherits]-(s) return s", {"props": story, "uname": author, "pid": incoming["padre"]})
+    tagDict = {}
+    miQuery = "match (a:Author) " +\
+              "match (p:Story) " +\
+              "where a.username = $uname and id(p) = $pid " +\
+              "create (s :Story $props), (a) -[:Wrote]->(s), (p)<-[:Inherits]-(s) "
 
+    for tag in tags:
+        miQuery += "merge ({0}:Tag {{ name: ${0}}}) ".format(tag.replace("#", ""))
+        tagDict[tag.replace("#", "")] = tag
+        miQuery += "create (s)-[:Is]->("+tag.replace("#", "")+") "
+
+    miQuery += "return s"
+    print(miQuery)
+    parameters = {"props": story, "uname": author, "pid": incoming["padre"]}
+    print({**tagDict, **parameters})
+
+    resp = query(miQuery, {**tagDict, **parameters})
+    print(resp)
     return Response(json.dumps(resp), content_type='application/json')
 
 
@@ -30,8 +48,8 @@ def getRootId():
 @story.route('/<storyId>')
 def getStory(storyId):
     if not storyId.isdigit():
-        return Response("Story id is not a number", status = 400)
-        
+        return Response("Story id is not a number", status=400)
+
     resp = query("match (n:Story) where id(n) = $sid return n",
                  {"sid": int(storyId)})
     return Response(json.dumps(resp), content_type='application/json')
