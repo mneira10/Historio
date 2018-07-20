da44cf8c6ab99afd82d833f2d49776413bc2116b
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-07-19 23:44:33 -0500

    rating crud implemented except gets

--- back/REST/rating.py
+++ back/REST/rating.py
@@ -4,6 +4,34 @@ from aux import *
 rating = Blueprint("rating",__name__)
 
 
-@rating.route('/')
-def main():
-    return "rating"
+@rating.route('/', methods = ["POST"])
+@authorExists
+def createRating():
+    incoming = request.json
+    story = incoming["story"]
+    author = incoming["author"]["username"]
+    rating = incoming["rating"]
+    resp = query("match (a:Author) match (s:Story) where a.username = $uname and id(s) = $sid merge (s)<-[r:Rating ]-(a) on create set r.rating = $rating, r.comment = $comment, r.date = $date return r",
+        {"uname":author,"sid":story, "rating":rating["rating"],"comment":rating["comment"],"date":rating["date"]})
+    return Response(json.dumps(resp), content_type='application/json')
+
+
+@rating.route('/', methods = ["PUT"])
+def updateRating():
+    incoming = request.json
+    story = incoming["story"]
+    author = incoming["author"]["username"]
+    rating = incoming["rating"]
+    resp = query(" match (s:Story)<-[r:Rating ]-(a:Author) where a.username = $uname and id(s) = $sid set r.rating = $rating, r.comment = $comment, r.date = $date return r",
+        {"uname":author,"sid":story, "rating":rating["rating"],"comment":rating["comment"],"date":rating["date"]})
+    return Response(json.dumps(resp), content_type='application/json')
+
+
+@rating.route('/', methods = ["DELETE"])
+def deleteRating():
+    incoming = request.json
+    story = incoming["story"]
+    author = incoming["author"]["username"]
+    resp = query("match (s:Story)<-[r:Rating ]-(a:Author) where a.username = $uname and id(s) = $sid delete r",
+        {"uname":author,"sid":story})
+    return Response(json.dumps(resp), content_type='application/json')
