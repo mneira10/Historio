b66156a581ea4014efabb267cf3bbfbcc462192c
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-09-07 15:43:31 -0500

    moved files to sepparate folder, migrating everything to express

---
+++ back/REST/flask/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_cors import CORS
+
+from author import author
+from followers import follower
+from rating import rating
+from story import story
+
+app = Flask(__name__)
+
+# module registration
+app.register_blueprint(story, url_prefix='/story')
+app.register_blueprint(author, url_prefix='/author')
+app.register_blueprint(rating, url_prefix='/rating')
+app.register_blueprint(follower, url_prefix='/followers')
+
+# enable port communication
+CORS(app)
+PORT = 8080
+DEBUG = False
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=PORT, debug=DEBUG, threaded=True)
---
+++ back/REST/flask/author.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, request, Response
+from aux import *
+
+author = Blueprint("author", __name__)
+
+
+@author.route('/', methods=['POST'])
+def createAuthor():
+    incoming = request.json
+    query("create (:Author $props)", {"props": incoming})
+    return getAuthor(incoming["username"])
+
+
+@author.route('/', methods=['PUT'])
+def updateAuthor():
+    incoming = request.json
+    query("match (n:Author) where n.username = $uname set n = $props",
+          {"props": incoming, "uname": incoming["username"]})
+    return getAuthor(incoming["username"])
+
+
+@author.route('/<author>')
+def getAuthor(author):
+    resp = query("match (n:Author) where n.username = $uname return n", {"uname": author})
+    return Response(json.dumps(resp), content_type='application/json')
+
+
+# author stories
+
+@author.route('/<author>/stories')
+def getAuthorStories(author):
+    resp = query("match (a:Author)-[:Wrote]->(s:Story) where a.username = $uname return s.title, s.date, id(s)",
+                 {"uname": author}, returnType="row")
+    return Response(json.dumps(resp), content_type='application/json')
