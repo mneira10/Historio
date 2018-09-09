11e2518a3c7f32e421f73757c4a77d57c2c54376
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-09-09 15:02:18 -0500

    all story requirements done exept create story

---
+++ back/REST/xpress/routes/story.mjs
@@ -0,0 +1,70 @@
+/*jslint devel: true */
+// "use strict";
+
+import express from "express";
+import {query} from "../dbConnectivity/query.mjs"
+const router = new express.Router();
+
+router.get("/root", (req, res) => {
+    query("match (N:Root) return id(N)", {
+        "": ""
+    }, "row", (data) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.json(data.results[0].data[0].row[0]);
+    });
+});
+
+router.get("/",(req,res)=>{
+    query("match (N:Story) return N", {
+        "": ""
+    }, "row", (data) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.json(data);
+    });
+})
+
+router.get("/:id",(req,res)=>{
+    query("match (N:Story) where id(N) = $sid return N", {
+        sid : parseInt(req.params.id)
+    }, "row", (data) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.json(data);
+    });
+});
+
+// story authors
+router.get("/:id/authors",(req,res)=>{
+    query("MATCH (s:Story ),(r:Root ) where id(s)=$sid match  p = allShortestPaths((s)-[:Inherits*]->(r)) match (a:Author)-[:Wrote]->(n) RETURN distinct a;", {
+        sid : parseInt(req.params.id)
+    }, "row", (data) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.json(data);
+    });
+});
+
+// story ratings
+router.get("/:id/ratings",(req,res)=>{
+    query("match (s:Story)<-[r:Rating]-(a:Author) where id(s)=$sid return r,a", {
+        sid : parseInt(req.params.id)
+    }, "row", (data) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.json(data);
+    });
+});
+
+// story tags
+
+router.get("/:id/tags",(req,res)=>{
+    query("match (s:Story)-[:Is]->(t:Tag) where id(s)=$sid return t", {
+        sid : parseInt(req.params.id)
+    }, "row", (data) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.json(data);
+    });
+});
+
+// TODO: create story
+
+export {
+    router
+};
