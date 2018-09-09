b12881814507e16b6044f2a4cc87d469f36aaafa
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-09-09 13:22:47 -0500

    succesfull connection to db query manages that

---
+++ back/REST/xpress/dbConnectivity/query.mjs
@@ -0,0 +1,50 @@
+/*jslint devel: true */
+// "use strict";
+
+
+import btoa from 'btoa';
+import request from "request";
+
+
+let headersToSend = {
+    'Authorization': 'Basic ' + b64EncodeUnicode('flask:2hn54frYm4', 'utf-8'),
+    'Accept': 'application/json; charset=UTF-8',
+    'Content-Type': 'application/json'
+}
+
+
+function query(theQuery, JSONparam = {
+    "": ""
+}, returnType = "graph", cbk) {
+    let config = {
+        "statements": [{
+            "statement": theQuery,
+            "parameters": JSONparam,
+            "resultDataContents": [returnType]
+        }]
+    }
+    // url to db: http://neo4jbig8575.cloudapp.net
+
+    request({
+        url: "http://neo4jbig8575.cloudapp.net:7474/db/data/transaction/commit",
+        method: "POST",
+        headers: headersToSend,
+        // formData: JSON.stringify(config),
+        json: config
+    }, function (error, response, body) {
+        console.log("va a imprimir");
+        // console.log(response);
+        cbk(body);
+    });
+
+
+}
+
+function b64EncodeUnicode(str) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+        function toSolidBytes(match, p1) {
+            return String.fromCharCode('0x' + p1);
+        }));
+}
+
+export {query};
--- back/REST/xpress/routes/author.mjs
+++ back/REST/xpress/routes/author.mjs
@@ -2,50 +2,19 @@
 // "use strict";
 
 import express from "express";
-import btoa from 'btoa';
-import $ from 'jquery';
+import {query} from "../dbConnectivity/query.mjs"
 const router = new express.Router();
 
-let headersToSend = {
-    'Authorization': 'Basic ' + b64EncodeUnicode('flask:2hn54frYm4', 'utf-8'),
-    'Accept': 'application/json; charset=UTF-8',
-    'Content-Type': 'application/json'
-}
-
-function query(theQuery, JSONparam = { "" : "" } , returnType="graph",cbk) {
-    let config = {
-        "statements": [
-            {
-                "statement": theQuery,
-                "parameters": JSONparam,
-                "resultDataContents": [returnType]
-            }
-        ]
-    }
-    // url to db: http://neo4jbig8575.cloudapp.net
-
-    $.ajax({
-        url: 'http://neo4jbig8575.cloudapp.net',
-        type: 'post',
-        data: config,
-        headers: headersToSend,
-        dataType: 'json',
-        success: ((data) => cbk(data) )
-    });
-}
-
-function b64EncodeUnicode(str) {
-    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
-        function toSolidBytes(match, p1) {
-            return String.fromCharCode('0x' + p1);
-    }));
-}
-
 router.get("/", (req, res) => {
-    query("match (n) return n" , (data) => console.log(data));
+    query("match (N:Root) return id(N)", {
+        "": ""
+    }, "row", (data) => {
+        console.log("da cbk");
+        res.set('Content-Type', 'application/json');
+        res.send(data);
+    });
 });
 
-export {router};
-
-
-
+export {
+    router
+};
