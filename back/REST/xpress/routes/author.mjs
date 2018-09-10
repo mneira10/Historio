f3248ffc271137386a19b2cbcf6ea7ca5d9a59d5
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-09-09 22:34:29 -0500

    create author done

--- back/REST/xpress/routes/author.mjs
+++ back/REST/xpress/routes/author.mjs
@@ -2,9 +2,14 @@
 // "use strict";
 
 import express from "express";
-import {query} from "../dbConnectivity/query.mjs"
+import bodyParser from "body-parser";
+import {
+    query
+} from "../dbConnectivity/query.mjs"
 const router = new express.Router();
 
+router.use(bodyParser.json()); 
+router.use(bodyParser.urlencoded({ extended: true }));
 
 // all authors
 router.get("/", (req, res) => {
@@ -20,7 +25,7 @@ router.get("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
     query("match (N:Author) where id(N) = $sid return N", {
-        sid : parseInt(req.params.id)
+        sid: parseInt(req.params.id)
     }, "row", (data) => {
         res.setHeader('Content-Type', 'application/json');
         res.json(data);
@@ -31,13 +36,46 @@ router.get("/:id", (req, res) => {
 
 router.get("/:authorName/stories", (req, res) => {
     query("match (a:Author)-[:Wrote]->(s:Story) where a.username = $uname return s.title, s.date, id(s)", {
-        uname : req.params.authorName
+        uname: req.params.authorName
     }, "row", (data) => {
         res.setHeader('Content-Type', 'application/json');
         res.json(data);
     });
 });
 
+function authorExists(req, res, uname, f) {
+    query("match (n:Author) where n.username = $usrname return n", {
+        usrname: uname
+    }, "graph", (data) => {
+        // console.log(data.results[0].data);
+        if (data.results[0].data.length > 0) {
+            res.status(400).send("Author already exists");
+        } else {
+            f();
+        }
+    });
+}
+
+router.post("/", (req,res)=>{
+    let uname = req.body.username;
+    // console.log(uname);
+    authorExists(req,res,uname,()=>{
+        //author doesnt exist, creemosl@! 
+        query("create (:Author $props)", {
+            props: req.body
+        }, "row", () => {
+            //once inserted, return the inserted author
+            //this verifies proper insertion
+            query("match (n:Author) where n.username = $usrname return n", {
+                usrname: uname
+            }, "row", (data) => {
+                res.json(data);
+            });
+        });
+    });
+});
+
+//delete author not supported for the time being
 
 export {
     router
