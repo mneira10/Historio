d77d1969fb0bc684af00fd6e7464defe7c6600aa
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-09-16 17:55:57 -0500

    added user endpoint

--- back/REST/xpress/index.mjs
+++ back/REST/xpress/index.mjs
@@ -4,6 +4,7 @@
 import {router as author}  from "./routes/author";
 import {router as story}  from "./routes/story";
 import {router as rating}  from "./routes/rating";
+import {router as user}  from "./routes/user";
 import express from "express";
 
 const app = express();
@@ -11,6 +12,7 @@ const app = express();
 app.use("/author", author);
 app.use("/story", story);
 app.use("/rating", rating);
+app.use("/user", user);
 
 
 
