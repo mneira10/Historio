ff07dbca008ba437bea3ccd220bf4761c3e1a674
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-09-17 05:44:20 -0500

    aesthetics more similar to the rest of the page

--- front/src/createStory/createStory.js
+++ front/src/createStory/createStory.js
@@ -5,6 +5,9 @@ import { dateToString } from "../util";
 import { Cookies, withCookies } from "react-cookie";
 import { instanceOf } from "prop-types";
 import {root} from "../App";
+import "../main/resources/styles/main.css";
+import "../story/resources/styles/story.css";
+import "../story/resources/styles/filter.css";
 
 class CreateStory extends Component {
 
@@ -38,34 +41,34 @@ class CreateStory extends Component {
 
   render() {
     return (
-      <div style={{ backgroundColor: "#455059", height: "100%", paddingTop: "30px", width: "100%" }}>
-        <h1 style={{ textAlign: "center", color: "white", fontSize: "400%", margin: "0 0" }}>New Story</h1>
-        <p style={{ color: "white", padding: "0 10%", fontSize: "200%", margin: "0 0" }}><b>Title</b></p>
+      <div style={{ backgroundColor: "white", height: "100%", paddingTop: "30px", width: "100%" }}>
+        <h1 style={{ textAlign: "center", color: "#212121", fontSize: "400%", margin: "0 0" }}>New Story</h1>
+        <p style={{ color: "#212121", padding: "0 10%", fontSize: "200%", margin: "0 0" }}><b>Title</b></p>
         <input type="text"
           style={{
-            margin: "1%  10%", fontSize: "200%", color: "white", backgroundColor: "transparent", border: "none",
-            borderBottom: "2px solid white"
+            margin: "1%  10%", fontSize: "200%", color: "#212121", backgroundColor: "transparent", border: "none",
+            borderBottom: "2px solid #212121"
           }} placeholder="Your title goes here" onChange={( e ) => this.story.title = e.target.value}/>
 
         <div style={{ textAlign: "center", height: "60%" }}>
           <textarea style={{
             width: "80% ",
             height: "95%",
-            color: "white",
+            color: "#212121",
             backgroundColor: "transparent",
-            border: "2px solid white",
+            border: "2px solid #212121",
             fontSize: "150%"
-          }} name="Your story" wrap="hard" onChange={( e ) => this.story.text = e.target.value}>
-        Type your story here!
+          }} placeholder="Type your story here!" name="Your story" wrap="hard" onChange={( e ) => this.story.text = e.target.value}>
+        
           </textarea>
         </div>
 
-        <p style={{ color: "white", padding: "0 10%", fontSize: "200%", margin: "0 0" }}><b>Tags</b></p>
+        <p style={{ color: "#212121", padding: "0 10%", fontSize: "200%", margin: "0 0" }}><b>Tags</b></p>
         <input type="text"
           style={{
             margin: "1%  10%",
             fontSize: "200%",
-            color: "white",
+            color: "#212121",
             backgroundColor: "transparent",
             border: "none",
             width: "80%"
@@ -73,7 +76,7 @@ class CreateStory extends Component {
           placeholder="Tag your story with as many tags as you want! (separated by spaces and without #)"
           onChange={( e ) => this.story.tags = e.target.value}/>
         <div style={{ textAlign: "center" }}>
-          <button onClick={this.create}>Submit!</button>
+          <button id='submitButton' style={{backgroundColor:"grey",borderRadius:"10%",fontSize:'200%'}}  onClick={this.create} >Submit</button>
         </div>
       </div>
     );
