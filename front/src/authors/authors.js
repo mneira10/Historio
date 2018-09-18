fb7d95d65ca9301268480619fa7061fa222b804c
Laura Valeria Vanegas García <lv.vanegas10@uniandes.edu.co>
2018-09-18 07:42:12 -0500

    Update authors.js

--- front/src/authors/authors.js
+++ front/src/authors/authors.js
@@ -9,7 +9,8 @@ export class Authors extends Component {
 
   render() {
     return (
-      <div style={{width:'100%'}}>
+      // Dont use style here, import css sheet
+      <div id="autor-div">
         <h1 style={comingFeatureStyle}>Feature coming soon!</h1>
       </div>
     )
